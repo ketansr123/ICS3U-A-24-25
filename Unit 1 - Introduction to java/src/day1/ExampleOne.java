ab93c3d94098e19432b4ebe3f40ae47fa504315d
Ketan Rawat <krawat@bayviewglen.ca>
2024-09-06 09:42:57 -0400

    initial commit

---
+++ Unit 1 - Introduction to java/src/day1/ExampleOne.java
@@ -0,0 +1,12 @@
+package day1;
+
+/**
+ * ExampleOne
+ */
+public class ExampleOne {
+
+        public static void main(String[] args) {
+            System.out.println("Hello ICS3u!");
+        }
+    }
+    
